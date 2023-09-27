1d18674a62a6e421cc3f27d421062182f4edf3ef
Great Ochuko <greatochuko@gmail.com>
2023-09-27 22:31:57 +0100

    turn every clickable element to a react-router Link element

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "./Categories.module.css";
 import SectionHeader from "./SectionHeader";
 
@@ -6,34 +7,34 @@ export default function Categories() {
     <section className={styles.categories}>
       <SectionHeader title="Shop By Categories" link="See all Categories" />
       <div className={styles.categoriesContianer}>
-        <div className={styles.category}>
+        <Link className={styles.category}>
           <img
             src="computer-accessories-category.jpg"
             alt="Computer accessories Category"
           />
           <h3>Computer Accessories</h3>
-        </div>
-        <div className={styles.category}>
+        </Link>
+        <Link className={styles.category}>
           <img
             src="health-and-personal-care-catefory.jpg"
             alt="Health and personal care Category"
           />
           <h3>Health & Personal Care</h3>
-        </div>
-        <div className={styles.category}>
+        </Link>
+        <Link className={styles.category}>
           <img
             src="beauty-products-category.jpg"
             alt="Beauty products Category"
           />
           <h3>Beauty Products</h3>
-        </div>
-        <div className={styles.category}>
+        </Link>
+        <Link className={styles.category}>
           <img
             src="sports-and-outdoors-category.jpg"
             alt="Sports and Outdoors category"
           />
           <h3>Sports and Outdoors</h3>
-        </div>
+        </Link>
       </div>
     </section>
   );
--- src/components/HotDeals.jsx
+++ src/components/HotDeals.jsx
@@ -1,36 +1,37 @@
+import { Link } from "react-router-dom";
 import styles from "./hotDeals.module.css";
 
 export default function QuickAccess() {
   return (
     <div className={styles.hotDeals}>
-      <div className={styles.hotDeal}>
+      <Link className={styles.hotDeal}>
         <img src="kitchen.jpg" alt="Kitchen category" />
         <div className={styles.text}>
           <h2>Kitchen Deals</h2>
           <p>Get 10% off</p>
         </div>
-      </div>
-      <div className={styles.hotDeal}>
+      </Link>
+      <Link className={styles.hotDeal}>
         <img src="toys.jpg" alt="Kitchen category" />
         <div className={styles.text}>
           <h2>Toys</h2>
           <p>Huge sale 30% off</p>
         </div>
-      </div>
-      <div className={styles.hotDeal}>
+      </Link>
+      <Link className={styles.hotDeal}>
         <img src="fashion.jpg" alt="Kitchen category" />
         <div className={styles.text}>
           <h2>Fashion Deals</h2>
           <p>Huge sale 25% off</p>
         </div>
-      </div>
-      <div className={styles.hotDeal}>
+      </Link>
+      <Link className={styles.hotDeal}>
         <img src="gaming-accessories.webp" alt="Kitchen category" />
         <div className={styles.text}>
           <h2>Shop Gaming Accessories</h2>
           <p>Get 15% off</p>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
