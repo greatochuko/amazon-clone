1a9b9166081c415941cf1936c629558ecb583def
Great Ochuko <greatochuko@gmail.com>
2023-09-28 23:39:57 +0100

    finished product information section in product details page

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,14 @@
 import styles from "./Rating.module.css";
 
-export default function Rating({ rating = 5, maxRating = rating, reviews }) {
+export default function Rating({
+  rating = 5,
+  maxRating = rating,
+  reviews,
+  style,
+}) {
   let numberArray = Array.from(Array(maxRating).keys());
   return (
-    <div className={styles.rating}>
+    <div className={styles.rating} style={style}>
       <div className={styles.stars}>
         {numberArray.map((a, index) =>
           rating > index ? (
